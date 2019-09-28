2b9fcbe789c1a191ab5ce06fa6ca42130a47b750
Alexis Tourneux <alexis.tourneux@supelec.fr>
2019-09-28 21:09:10 +0200

    Added stations as pytest fixtures to conftest

--- trainwarner_project/bloks/trainwarner/tests/conftest.py
+++ trainwarner_project/bloks/trainwarner/tests/conftest.py
@@ -1,2 +1,24 @@
 from anyblok.conftest import *  # noqa: F401,F403
-from anyblok_pyramid.conftest import *  # noqa: F401,F403
+import pytest
+
+
+@pytest.fixture
+def paris_nord_station(self, rollback_registry):
+
+	"""Insert station for Paris Nord."""
+
+	return rollback_registry.Station.insert(
+		name="Paris Nord",
+		slug="paris-nord",
+		id=16891)
+
+
+@pytest.fixture
+def lille_flandres_station(self, rollback_registry):
+
+	"""Insert station for Lille-Flandres."""
+
+	return rollback_registry.Station.insert(
+		name="Lille Flandres",
+		slug="lille-flandres",
+		id=123)
