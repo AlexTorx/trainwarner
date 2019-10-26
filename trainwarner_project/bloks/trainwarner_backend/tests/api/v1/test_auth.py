1a7ff823022bc89f226641839380d9f6de843b51
alexis <alexis.tourneux@zeprofile.com>
2019-10-26 10:56:27 +0200

    Added tests for authentication endpoint

---
+++ trainwarner_project/bloks/trainwarner_backend/tests/api/v1/test_auth.py
@@ -0,0 +1,82 @@
+import pytest
+
+
+@pytest.mark.usefixtures('webserver')
+class TestAuthLogin:
+
+    """This test class is aimed at checking that /backend/api/v1/auth/login API
+       endpoint is working as intented."""
+
+    def test_auth_login_available_methods(self, webserver):
+
+        """This test is aimed at checking that only POST method is available
+           on API endpoint /backend/api/v1/auth/login."""
+
+        # POST method on auth login endpoint with no parameters raises a 400
+        # Bad Request HTTP response
+        webserver.post_json(
+               '/backend/api/v1/auth/login',
+                dict(),
+                status=400
+                )
+
+        # All others methods will raised a 405 Method Not Available HTTP error
+        response = webserver.get(
+                '/backend/api/v1/auth/login',
+                status=405
+                )
+        assert response.status == '405 Method Not Allowed'
+
+        response = webserver.patch(
+                '/backend/api/v1/auth/login',
+                dict(),
+                status=405
+                )
+        assert response.status == '405 Method Not Allowed'
+
+        response = webserver.put(
+                '/backend/api/v1/auth/login',
+                dict(),
+                status=405
+                )
+        assert response.status == '405 Method Not Allowed'
+
+        response = webserver.delete(
+                '/backend/api/v1/auth/login',
+                status=405
+                )
+        assert response.status == '405 Method Not Allowed'
+
+    def test_auth_login_success(self, webserver, admin_user):
+
+        """This test is aimed at checking that authentication is behaving as
+           intented."""
+
+        response = webserver.post_json(
+                '/backend/api/v1/auth/login',
+                dict(
+                    login=admin_user.login,
+                    password="1234"
+                    )
+                )
+
+        assert response.status_code == 200
+        assert 'Set-Cookie' in response.headers.keys()
+
+    def test_auth_login_fails_wrong_credentials(self, webserver, admin_user):
+
+        """This test is aimed at checking that when wronf credentials are
+           provided, authentication fails and a 401 Unauthorized HTTP error is
+           raised."""
+
+        response = webserver.post_json(
+                '/backend/api/v1/auth/login',
+                dict(
+                    login="not_a_valid_login",
+                    password="not_a_valid_password"
+                    ),
+                status=401
+                )
+
+        assert response.status_code == 401
+        assert 'Set-Cookie' not in response.headers.keys()
---
+++ trainwarner_project/bloks/trainwarner_backend/tests/conftest.py
@@ -0,0 +1,62 @@
+from anyblok.conftest import *  # noqa: F401,F403
+from anyblok_pyramid.conftest import *  # noqa: F401,F403
+
+from anyblok_pyramid.testing import init_web_server
+
+import pytest
+
+
+@pytest.fixture
+def webserver(request, configuration_loaded):
+
+    """This fixture defines a webserver that can be used in tests for making
+       requests on API endpoints for instance."""
+
+    return init_web_server()
+
+
+@pytest.fixture
+def fixture_webserver(request, configuration_loaded):
+
+    """This fixture defines a webserver that is only intented to be used inside
+       fixtures that require making calls to API endpoints. Indeed, webserver
+       has a memory, especially for cookies and authentication tokens that may
+       not be suitable for any use."""
+
+    return init_web_server()
+
+
+@pytest.fixture
+def admin_user(rollback_registry):
+
+    """This fixture provides an user with credentials with common-admin role.
+    """
+
+    user = rollback_registry.User.insert(
+            email="admin@trainwarner.com", first_name="Alexis",
+            last_name="Tourneux", login="admin")
+    rollback_registry.User.CredentialStore.insert(
+            login=user.login, password="1234")
+
+    admin_role = rollback_registry.User.Role.query().filter_by(
+            name="common-admin").first()
+    admin_role.users.append(user)
+
+    return user
+
+
+@pytest.fixture
+def authenticated_headers(rollback_registry, admin_user, fixture_webserver):
+
+    """This fixture is aimed at provided authenticated headers in order to
+       make requests on protected API endpoints in tests suite."""
+
+    response = fixture_webserver.post_json(
+           '/backend/api/v1/auth/login',
+            dict(
+                login=admin_user.login,
+                password="1234"
+                )
+            )
+
+    return {'Set-Cookie': response.headers.get('Set-Cookie')}
